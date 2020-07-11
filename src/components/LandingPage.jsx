09837d41188b5ec0fe5ad78d1f9aea36e25e0c96
Clayton Nishitani-Hart <claytonnishitanihart@gmail.com>
2020-07-12 08:48:41 +1200

    changed logo, added weather app

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,23 +1,28 @@
 import React from "react";
 import "./LandingPage.css";
 import ProjectCard from "./ProjectCard";
-import Logo from "./../fadsf.png";
+import Logo from "./../images/face.png";
 import TomNook from "./../images/tom_nook.png";
 import NooksCranny from "./../images/nooks_cranny.jpg";
 import DwightSchrute from "./../images/dwight_schrute.png";
 import TheOffice from "./../images/the_office.jpg";
 import Controller from "./../images/eiro.png";
 import Eiro from "./../images/eiro_bg.png";
+import Weather from "./../images/weather.png";
+import Bliss from "./../images/bliss.png";
+import { FaGithubSquare, FaLinkedin } from "react-icons/fa";
 
 class LandingPage extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            lightMode: true
+            lightMode: true,
+            showingSocials: true
         };
 
         this.toggleLightDarkMode = this.toggleLightDarkMode.bind(this);
         this.gotoPosition = this.gotoPosition.bind(this);
+        this.toggleSocials = this.toggleSocials.bind(this);
     }
     componentDidMount() {
         document.body.classList.add("light");
@@ -40,6 +45,19 @@ class LandingPage extends React.Component {
         element.scrollIntoView({behavior: "smooth"});
     }
 
+    toggleSocials() {
+        let element = document.getElementById("socials-panel");
+        this.setState({showingSocials: !this.state.showingSocials});
+        console.log(this.state.showingSocials);
+        if (this.state.showingSocials) {
+            element.style.height = "200px";
+            element.style.opacity = "1";
+        } else {
+            element.style.height = "0";
+            element.style.opacity = "0";
+        }
+    }
+
     render() {
         return (
             <div>
@@ -55,9 +73,21 @@ class LandingPage extends React.Component {
                 <div className={"LandingPage-Content"}>
                     <h2 id={"Projects"}>Projects</h2>
                     <div className={"LandingPage-Projects"}>
-                        <ProjectCard projectName={"Nook's Cranny ecommerce shop"} projectDescription={"An ecommerce website based on Nook's Cranny."} projectImage={TomNook} projectBackground={NooksCranny} projectLink={"https://nooks-cranny.vercel.app/"} projectButtonName={"Link"} needBorder={false}/>
-                        <ProjectCard projectName={"Dwight Schrute Markov Twitter bot"} projectDescription={"A twitter bot that responds to mentions that contain the #dwigtschrudequote."} projectImage={DwightSchrute} projectBackground={TheOffice} projectLink={"https://github.com/claytonNishitaniHart/twitterbot"} projectButtonName={"Link"} needBorder={true}/>
-                        <ProjectCard projectName={"Eiro"} projectDescription={"Eiro was my final year project that another programmer, three artists, and I worked on for a full year."} projectImage={Controller} projectBackground={Eiro} projectLink={"https://jack3311.itch.io/eiro"} projectButtonName={"Link"} needBorder={false}/>
+                        <ProjectCard projectName={"Nook's Cranny ecommerce shop"} projectDescription={"An ecommerce website based on Nook's Cranny."} projectImage={TomNook} projectBackground={NooksCranny} projectLink={"https://nooks-cranny.vercel.app/"} needBorder={false}/>
+                        <ProjectCard projectName={"Dwight Schrute Markov Twitter bot"} projectDescription={"A twitter bot that responds to mentions that contain the #dwigtschrudequote."} projectImage={DwightSchrute} projectBackground={TheOffice} projectLink={"https://github.com/claytonNishitaniHart/twitterbot"} needBorder={true}/>
+                        <ProjectCard projectName={"Eiro"} projectDescription={"Eiro was my final year project that another programmer, three artists, and I worked on for a full year."} projectImage={Controller} projectBackground={Eiro} projectLink={"https://jack3311.itch.io/eiro"} needBorder={false}/>
+                        <ProjectCard projectName={"Weather App"} projectDescription={"A weather app that uses the user's location to get the weather for each day in a week."} projectImage={Weather} projectBackground={Bliss} projectLink={"https://weather-app-topaz-nine.vercel.app/"} needBorder={false}/>
+                    </div>
+                    <div className={"socials"} onClick={this.toggleSocials}>
+                        <img className={"socials-img"} src={Logo} alt={"socials"}/>
+                    </div>
+                    <div id={"socials-panel"} className={"socials-panel"}>
+                        <button className={"socials-button github"}>
+                            <FaGithubSquare className={"button-icon"}/>
+                        </button>
+                        <button className={"socials-button linkedin"}>
+                            <FaLinkedin className={"button-icon"}/>
+                        </button>
                     </div>
                 </div>
             </div>
--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -29,7 +29,7 @@ class ProjectCard extends React.Component{
                 <div id={"info"+ this.props.projectName} className={"Card-info"}>
                     <span className={this.props.needBorder ? "info-name need-border" : "info-name"}>{this.props.projectName}</span>
                     <span className={"info-description"}>{this.props.projectDescription}</span>
-                    <a className={"info-button"} href={this.props.projectLink} target={"_blank"} rel={"noopener noreferrer"}>{this.props.projectButtonName}</a>
+                    <a className={"info-button"} href={this.props.projectLink} target={"_blank"} rel={"noopener noreferrer"}>Link</a>
                 </div>
             </div>
         );
