8e937f70c7e0a5f17638b78ebf57a764bd6dd7c7
Clayton Nishitani-Hart <claytonnishitanihart@gmail.com>
2020-08-09 02:24:12 +1200

    added about me

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -80,7 +80,7 @@ class LandingPage extends React.Component {
                     </button>
                 </div>
                 <div className={"LandingPage-Content"}>
-                    <h2 id={"Projects"}>Projects</h2>
+                    <h2 id={"Projects"} className={"Title"}>Projects</h2>
                     <div className={"LandingPage-Projects"}>
                         <ProjectCard projectName={"PAINT: painting gallery"} projectDescription={"A gallery where I upload my paintings following Bob Ross videos, using React.js, SASS, HTML."} projectImage={BobRoss} bgGrad={"bg4"} projectDemoLink={"https://claytons-paintings.vercel.app/"} projectCodeLink={"https://github.com/claytonNishitaniHart/claytons-paintings"}/>
                         <ProjectCard projectName={"Claytinstagram: Instagram Clone"} projectDescription={"Followed a tutorial, improved on it by implementing likes and comments, using Laravel."} projectImage={Claytinstagram} bgGrad={"bg3"} projectDemoLink={"http://claytinstagram.herokuapp.com/"} projectCodeLink={"https://github.com/claytonNishitaniHart/claytinstagram"}/>
@@ -92,6 +92,8 @@ class LandingPage extends React.Component {
                         <ProjectCard projectName={"Eiro, (PC Game made with Unity)"} projectDescription={"Eiro was my final year project, it is a 3D adventure game, using C#, Unity3D."} projectImage={Controller} projectBackground={Eiro} projectDemoLink={"https://jack3311.itch.io/eiro"} projectCodeLink={null}/>
                         <ProjectCard projectName={"Weather Web App"} projectDescription={"A weather app that uses the user's location to get the weather, using React.js, CSS, REST API."} projectImage={Weather} projectBackground={Bliss} projectDemoLink={"https://weather-app-topaz-nine.vercel.app/"} projectCodeLink={"https://github.com/claytonNishitaniHart/weather-app"}/>
                     </div>
+                    <h2 className={"Title"}>About Me</h2>
+                    <p className={"About-Me"}>I am an enthusiastic and detail-oriented aspiring web developer, with a Bachelor of Software Engineering degree from Media Design School. I consider myself a self-motivated learner, who has a strong desire to grow as a developer. I'm passionate about software development and strongly interested in obtaining an entry-level position in the web development field.</p>
                     <div className={"socials-container"}>
                         <div className={"socials"} onClick={this.toggleSocials}>
                             <div className={"socials-clickme"}>
